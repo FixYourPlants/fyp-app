780cf454c7b667826685cc279c3e5509a2f66249
Alejandro Santiago Félix <alex.0002002@gmail.com>
2024-03-25 13:46:52 +0100

    feat: Added details

--- app/src/main/java/com/fyp/app/ui/screens/DetailScreen.kt
+++ app/src/main/java/com/fyp/app/ui/screens/DetailScreen.kt
@@ -1,29 +1,67 @@
 package com.example.myapplication.domain.home.screens
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.fyp.app.R
+import com.fyp.app.ui.components.BoxTag
 import com.fyp.app.ui.components.image.OverlayImageWithClick
 
-data class Plant(val name: String, val description: String, val imageUrl: Int)
+data class Plant(
+    val name: String,
+    val description: String,
+    val imageUrl: Int,
+    val characteristics: List<String>,
+    val dificulty: String = "Fácil",
+    val treatments: List<String>,
+    val sickness: List<String>,
+    val opinions: List<String>,
+    val scienceName: String
+)
 
 val plantDetails = Plant(
     name = "Lirio de la paz",
     description = "El lirio de la paz es una planta de interior popular por sus hojas verdes y brillantes.",
-    imageUrl = R.drawable.down_down
+    imageUrl = R.drawable.down_down,
+    characteristics = listOf(
+        "Planta de interior",
+        "Fácil de cuidar",
+        "No necesita luz directa"
+    ),
+    dificulty = "Fácil",
+    treatments = listOf(
+        "Riego moderado",
+        "Evitar corrientes de aire",
+        "Fertilizar cada 2 semanas"
+    ),
+    sickness = listOf(
+        "Hojas amarillas",
+        "Pudrición de raíces"
+    ),
+    opinions = listOf(
+        "Me encanta esta planta",
+        "Fácil de cuidar",
+        "Muy bonita"
+    ),
+    scienceName = "Spathiphyllum wallisii"
 )
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -32,52 +70,102 @@ fun PlantDetailsScreen(
     plant: Plant,
     onBackClicked: () -> Unit
 ) {
-    Column(
+    LazyColumn(
         modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
+            .background(Color.White)
             .padding(16.dp)
     ) {
-        TopAppBar(
-            title = {
-                Text(
-                    text = plant.name
-                )
-            },
-            navigationIcon = {
-                IconButton(onClick = { onBackClicked() }) {
-                    Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Back")
-                }
-            }
-        )
-
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(8.dp)
-        ) {
-            item {
-                Column {
-                    // Utiliza el composable OverlayImageWithClick
+        item {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(
+                    modifier = Modifier.weight(0.5f)
+                ) {
                     OverlayImageWithClick(
                         defaultImageUrl = plant.imageUrl,
                         clickedImageUrl = R.drawable.down_down,
                         onClick = { /* Acciones al hacer clic en la imagen */ }
                     )
                 }
-
-            }
-            item {
-                Column {
+                Spacer(modifier = Modifier.width(16.dp))
+                Column(
+                    modifier = Modifier.weight(0.5f)
+                ) {
+                    Text(
+                        fontWeight = FontWeight.Bold,
+                        text = plant.name,
+                        fontSize = 24.sp,
+                        color = Color.Black
+                    )
+                    Text(
+                        text = plant.scienceName,
+                        fontStyle = FontStyle.Italic,
+                        fontSize = 12.sp,
+                        modifier = Modifier.padding(vertical = 8.dp)
+                    )
                     Text(
                         text = plant.description,
-                        modifier = Modifier.padding(vertical = 8.dp)
+                        modifier = Modifier.padding(vertical = 8.dp),
+                        color = Color.Black
+                    )
+                    Text(
+                        text = "Dificultad: ",
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black
+                    )
+                    Text(
+                        text = plant.dificulty,
+                        color = Color.Black
                     )
                 }
             }
         }
+        item {
+            Spacer(modifier = Modifier.height(16.dp))
+            BoxTag(name = "Características", values = plant.characteristics)
+        }
+        item {
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "Cuidado recomendado",
+                fontWeight = FontWeight.Bold,
+                color = Color.Black
+            )
+            plant.treatments.forEach { treatment ->
+                Text(
+                    text = "• $treatment",
+                    modifier = Modifier.padding(vertical = 4.dp),
+                    color = Color.Black
+                )
+            }
+        }
+        item {
+            Spacer(modifier = Modifier.height(16.dp))
+            BoxTag(name = "Enfermedades:", values = plant.sickness)
+        }
+        item {
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "Opiniones",
+                fontWeight = FontWeight.Bold,
+                color = Color.Black
+            )
+            plant.opinions.forEach { opinion ->
+                Text(
+                    text = "• $opinion",
+                    modifier = Modifier.padding(vertical = 4.dp),
+                    color = Color.Black
+                )
+            }
+        }
     }
 }
+
+
+
 
 @Composable
 @Preview
