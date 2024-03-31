b567af9a2543fdd1ff8b0e7a6cb581107632d176
Ignarrman <nacho.arromant@gmail.com>
2024-03-31 20:51:01 +0200

    conf: Cambia build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,7 +90,6 @@ dependencies {
     ksp("io.github.raamcosta.compose-destinations:ksp:1.10.2")
     implementation("com.google.firebase:firebase-auth-ktx:21.1.0")
     implementation("com.google.android.gms:play-services-auth:20.4.1")
-
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.0")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.0")
     implementation("androidx.navigation:navigation-compose:2.5.3")
--- app/src/main/java/com/fyp/app/MainActivity.kt
+++ app/src/main/java/com/fyp/app/MainActivity.kt
@@ -43,8 +43,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-                DestinationsNavHost(navGraph = NavGraphs.root)
+            //DestinationsNavHost(navGraph = NavGraphs.root)
             ComposeGoogleSignTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
