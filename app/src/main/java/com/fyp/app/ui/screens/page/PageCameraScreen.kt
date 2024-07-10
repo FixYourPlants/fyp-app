fbe6e927921ef245373acdde2159566de15db11e
alesanfe <alex.0002002@gmail.com>
2024-07-10 13:45:54 +0200

    fix: Navigation error

--- app/src/main/java/com/fyp/app/ui/screens/page/PageCameraScreen.kt
+++ app/src/main/java/com/fyp/app/ui/screens/page/PageCameraScreen.kt
@@ -4,13 +4,15 @@ import androidx.compose.runtime.Composable
 import com.fyp.app.ui.components.Camera
 import com.fyp.app.viewmodel.camera.PageCameraViewModelImp
 import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
 @Composable
 @Destination
-fun PageCameraScreen() {
+fun PageCameraScreen(navigator: DestinationsNavigator) {
     val cameraViewModel = PageCameraViewModelImp.getInstance()
     Camera { bitmap ->
         cameraViewModel.onTakePhoto(bitmap)
         cameraViewModel.onSelectBitmap(bitmap)
+        navigator.navigateUp()
     }
 }
--- app/src/main/java/com/fyp/app/ui/screens/page/PageCreateScreen.kt
+++ app/src/main/java/com/fyp/app/ui/screens/page/PageCreateScreen.kt
@@ -31,7 +31,7 @@ fun PageCreateScreen(
     FormScreenContent(
         title = "Crear página en ${diary.title.lowercase()} el ${viewModel.formattedDate}",
         updatedImages = updatedImages,
-        onAddImageClick = { navigator.navigateUp() },
+        onAddImageClick = {  navigator.navigate(PageCameraScreenDestination()) },
         fields = viewModel.fields,
         errors = viewModel.errors,
         onSaveClick = {
