3e7a3105cbce3f01a61327162f310abee96c6020
alesanfe <alex.0002002@gmail.com>
2024-09-15 22:38:19 +0200

    refactor: Cleaned imports

--- app/src/main/java/com/fyp/app/ui/screens/PredictCameraScreen.kt
+++ app/src/main/java/com/fyp/app/ui/screens/PredictCameraScreen.kt
@@ -1,9 +1,6 @@
 package com.fyp.app.ui.screens
 
-import android.graphics.BitmapFactory
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
-import com.fyp.app.R
 import com.fyp.app.ui.components.Camera
 import com.fyp.app.viewmodel.camera.PredictCameraViewModelImp
 import com.ramcosta.composedestinations.annotation.Destination
@@ -13,7 +10,6 @@ import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 @Destination
 fun PredictCameraScreen(navigator: DestinationsNavigator) {
     val cameraViewModel = PredictCameraViewModelImp.getInstance()
-    val context = LocalContext.current
     Camera { bitmap ->
         cameraViewModel.onTakePhoto(bitmap)
         cameraViewModel.onSelectBitmap(bitmap)
